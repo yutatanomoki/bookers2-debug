ed06d4411040f62ce4fff04db6f4754c2cdfe846
yutatanomoki <yuta3071@gmail.com>
2021-01-17 14:04:27 +0000

    [add]8

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,9 @@
 class NotificationMailer < ApplicationMailer
+  default from: 'no-replay@gmail.com'
+
+  def complete_mail(user)
+    @user = user
+    @url = "http://localhost:3000/users/#{@user.id}"
+    mail(subject: "COMPLETE join your address" ,to: @user.email)
+  end
 end
